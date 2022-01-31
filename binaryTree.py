5be1524a23c309e3115242f5919706cf232d33e9
Himanshu Tomar <himanshutomar3031@gmail.com>
2022-01-31 22:30:01 +0530

    Deletion in binary tree

--- binaryTree.py
+++ binaryTree.py
@@ -28,7 +28,43 @@ class BinaryTree:
                 else:
                     queue.append(temp.right)
 
+    def delete(self, key):
+        if self.root == None or self.root.data == key:
+            self.root = None
+            return
+        else:
+            queue = []
+            node_to_be_deleted = None
+            leaf = None
+            queue.append(self.root)
+            while len(queue) != 0:
+                temp = queue.pop(0)
+                if temp.left is None and temp.right is None:
+                    leaf = temp
+                if temp.left is not None and temp.left.data == key:
+                    node_to_be_deleted = temp.left
+                if temp.left is not None:
+                    queue.append(temp.left)
+                if temp.left is not None and temp.right.data == key:
+                    node_to_be_deleted = temp.right
+                if temp.right is not None:
+                    queue.append(temp.right)
 
+            queue.append(self.root)
+            while len(queue) != 0:
+                temp = queue.pop(0)
+                if temp.left.data == leaf.data:
+                    temp.left = None
+                    break
+                else:
+                    queue.append(temp.left)
+                if temp.right.data == leaf.data:
+                    temp.right = None
+                    break
+                else:
+                    queue.append(temp.right)
+
+            node_to_be_deleted.data = leaf.data
 
 def inorder(temp):
     if (not temp):
@@ -52,6 +88,11 @@ key = 12
 tree.insert(12)
 
 
+print()
+print("Inorder traversal after insertion:", end=" ")
+inorder(tree.root)
+
+tree.delete(15)
 print()
 print("Inorder traversal after insertion:", end=" ")
 inorder(tree.root)
